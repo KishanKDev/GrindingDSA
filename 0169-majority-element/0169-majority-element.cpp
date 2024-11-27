1e3b75d83ebd2403824b870f1aae08af65a06a78
Kishan Kumar <141938467+KishanKDev@users.noreply.github.com>
2024-11-27 23:59:33 +0530

    Time: 0 ms (100%), Space: 28.1 MB (25.96%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -5,20 +5,50 @@ public:
 
       
 
-      for(int i=0;i<n;i++) {
-        int count= 0;
-        for(int j=0;j<n;j++){
-            if(nums[i]==nums[j]){
-                count++;
-            }
+    //   for(int i=0;i<n;i++) {
+    //     int count= 0;
+    //     for(int j=0;j<n;j++){
+    //         if(nums[i]==nums[j]){
+    //             count++;
+    //         }
+    //     }
+
+    //     if(count>n/2){
+    //         return nums[i];
+    //     }
+    //   }
+
+    //   return -1;
+
+    int count=0;
+    int el;
+
+    for(int i=0;i<n;i++){
+        if(count==0){
+            count=1;
+            el=nums[i];
+        }
+        else if(nums[i]==el){
+            count++;
+        }
+        else{
+            count--;
         }
 
-        if(count>n/2){
-            return nums[i];
-        }
-      }
 
-      return -1;
-       
+    }
+    int count1=0;
+    for(int i=0;i<n;i++){
+        if(nums[i]==el){
+            count1++;
+        }
+    
+    }
+
+    if(count1>n/2){
+        return el;
+    }
+
+    return -1;  
     }
 };
