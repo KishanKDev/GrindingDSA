5ff8994bc89ae722f38ee77115e164fa8dbf6c6b
Kishan Kumar <141938467+KishanKDev@users.noreply.github.com>
2024-12-12 23:25:52 +0530

    Time: 1196 ms (5.15%), Space: 17.4 MB (7.74%) - LeetHub

---
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        while (k-- > 0) {
+        // Sort the gifts array in descending order
+        sort(gifts.rbegin(), gifts.rend());
+
+        // Take the top pile and update it
+        gifts[0] = floor(sqrt(gifts[0]));
+    }
+
+    // Calculate the sum of all remaining gifts
+    long long totalRemaining = 0;
+    for (int gift : gifts) {
+        totalRemaining += gift;
+    }
+
+    return totalRemaining;
+    }
+};
