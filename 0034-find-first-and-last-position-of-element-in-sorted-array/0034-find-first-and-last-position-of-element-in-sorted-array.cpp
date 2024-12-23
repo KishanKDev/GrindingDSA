e0c31b0671b88216693eae0386a48345c10701af
Kishan Kumar <141938467+KishanKDev@users.noreply.github.com>
2024-12-24 00:34:12 +0530

    Time: 0 ms (100%), Space: 18.3 MB (8.53%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,36 +1,53 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> result = {-1, -1};
-        int left = binarySearch(nums, target, true);
-        int right = binarySearch(nums, target, false);
-        result[0] = left;
-        result[1] = right;
-        return result;        
+      vector<int>result={-1,-1};
+      result[0]=firstOccur(nums,target);
+      result[1]=lastOccur(nums,target);
+      return result;
     }
 
-    int binarySearch(vector<int>& nums, int target, bool isSearchingLeft) {
-        int left = 0;
-        int right = nums.size() - 1;
-        int idx = -1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                idx = mid;
-                if (isSearchingLeft) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+    int firstOccur(vector<int>nums,int target){
+        int n= nums.size();
+        int start=0, end= n-1;
+        int mid= start+(end-start)/2;
+        int ans=-1;
+
+        while(start<=end){
+            if(target== nums[mid]){
+                ans= mid;
+                end= mid-1;
             }
+            else if(target>nums[mid]){
+                start=mid+1;
+            }
+            else if(target<nums[mid]){
+                end= mid-1;
+            }
+            mid= start +(end-start)/2;
         }
-        
-        return idx;
-    }    
+        return ans;
+    }
+
+    int lastOccur(vector<int>nums,int target){
+        int n= nums.size();
+        int start=0, end= n-1;
+        int mid= start+(end-start)/2;
+        int ans=-1;
+
+        while(start<=end){
+            if(target== nums[mid]){
+                ans= mid;
+                start= mid+1;
+            }
+            else if(target>nums[mid]){
+                start=mid+1;
+            }
+            else if(target<nums[mid]){
+                end= mid-1;
+            }
+            mid= start +(end-start)/2;
+        }
+        return ans;
+    }
 };
