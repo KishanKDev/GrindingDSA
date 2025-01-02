10eb7830913938861b52b96bb7edefa2ff46bd11
Kishan Kumar <141938467+KishanKDev@users.noreply.github.com>
2025-01-02 22:29:51 +0530

    Time: 1 ms (14.16%), Space: 30.3 MB (16.87%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,28 +2,35 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n= nums.size();
-        if (n == 0)
-        return;
-         k = k % n;
-         if (k>n)
+        // if (n == 0)
+        // return;
+        //  k = k % n;
+        //  if (k>n)
 
-         return;
+        //  return;
 
 
-        // int temp[k]; 
-        vector<int> temp(k);
-        for (int i = n - k; i < n; i++)
-        {
-           temp[i - n + k] = nums[i];
+        // // int temp[k]; 
+        // vector<int> temp(k);
+        // for (int i = n - k; i < n; i++)
+        // {
+        //    temp[i - n + k] = nums[i];
+        // }
+        // for (int i = n - k - 1; i >= 0; i--)
+        // {
+        //    nums[i + k] = nums[i];
+        // }
+        // for (int i = 0; i < k; i++)
+        // {
+        //    nums[i] = temp[i];
+        // }
+
+        vector<int>ans(n);
+        
+        for(int Index=0;Index<n;Index++){
+            int NewIndex= (Index+k)% n;
+            ans[NewIndex]=nums[Index];
         }
-        for (int i = n - k - 1; i >= 0; i--)
-        {
-           nums[i + k] = nums[i];
-        }
-        for (int i = 0; i < k; i++)
-        {
-           nums[i] = temp[i];
-        }
-     
+        nums= ans;
     }
 };
