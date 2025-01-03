72823c85d202a17f19609655569fc7dc536a53e3
Kishan Kumar <141938467+KishanKDev@users.noreply.github.com>
2025-01-03 14:36:52 +0530

    Time: 0 ms (100%), Space: 14.9 MB (25.5%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,13 +3,36 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n=nums.size();
 
+        // for(int i=0;i<n;i++){
+        // for(int j=i+1;j<n;j++){
+        //     if(nums[i]+nums[j]==target){
+        //         return {i,j};
+        //     }
+        // }
+        // }
+        // return {-1,-1};
+        vector<pair<int, int>>indexnums;
+
         for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(nums[i]+nums[j]==target){
-                return {i,j};
+            indexnums.push_back({nums[i],i});
+        }
+
+        sort(indexnums.begin(),indexnums.end());
+        int low= 0;
+        int high= n-1;
+
+        while(low<high){
+            int currsum=indexnums[low].first+indexnums[high].first;
+            if(currsum==target){
+                return {indexnums[low].second,indexnums[high].second};
+            }
+            else if(currsum>target){
+                high--;
+            }
+            else{
+                low++;
             }
         }
-        }
-        return {-1,-1};
+        return{-1,-1};
     }
 };
