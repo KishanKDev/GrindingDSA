3dd9d62faeacb7d60dc07951d2f3fa30dc3a69a8
Kishan Kumar <141938467+KishanKDev@users.noreply.github.com>
2025-01-15 15:31:26 +0530

    Time: 11 ms (59.93%), Space: 21.2 MB (34.48%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int Expand(string s,int i,int j){
+        int count=0;
+
+        while(i>=0 && j<=s.length() && s[i]==s[j]){
+            count++;
+            i--;
+            j++;
+        }
+        return count;
+    }
+    int countSubstrings(string s) {
+        int TotalCount=0;
+
+        for(int i=0;i<s.length();i++){
+            int j=i;
+            int OddkaAns=Expand(s,i,j);
+
+             j= i+1;
+            int EvenkaAns= Expand(s,i,j);
+
+            TotalCount=TotalCount+OddkaAns+EvenkaAns;
+        }
+        return TotalCount;
+        
+    }
+};
