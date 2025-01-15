4eb2618ffd2ac8264a2f21d3471f4895445341c1
Kishan Kumar <141938467+KishanKDev@users.noreply.github.com>
2025-01-15 15:37:48 +0530

    Time: 7 ms (70.35%), Space: 21.2 MB (34.48%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -3,7 +3,7 @@ public:
     int Expand(string s,int i,int j){
         int count=0;
 
-        while(i>=0 && j<=s.length() && s[i]==s[j]){
+        while(i>=0 && j<s.length() && s[i]==s[j]){
             count++;
             i--;
             j++;
@@ -17,7 +17,7 @@ public:
             int j=i;
             int OddkaAns=Expand(s,i,j);
 
-             j= i+1;
+            j= i+1;
             int EvenkaAns= Expand(s,i,j);
 
             TotalCount=TotalCount+OddkaAns+EvenkaAns;
