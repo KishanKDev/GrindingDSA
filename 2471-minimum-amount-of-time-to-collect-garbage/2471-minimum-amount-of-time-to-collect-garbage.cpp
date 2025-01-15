fa15bfa01c5f54920a67226b22095feb268502f8
Kishan Kumar <141938467+KishanKDev@users.noreply.github.com>
2025-01-15 23:18:42 +0530

    Time: 18 ms (68.11%), Space: 107.2 MB (26.43%) - LeetHub

---
+++ 2471-minimum-amount-of-time-to-collect-garbage/2471-minimum-amount-of-time-to-collect-garbage.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        int pickP=0,pickG=0,pickM=0;
+
+        int travelP=0, travelG=0,travelM=0;
+
+        int lastP=0,lastG=0,lastM=0;
+
+        for(int i=0;i<garbage.size();i++){
+            string curr=garbage[i];
+
+            for(int j=0;j<garbage[i].length();j++){
+                char ch=curr[j];
+
+                if(ch=='P'){
+                    pickP++;
+                    lastP=i;
+                }
+                else if(ch=='G'){
+                    pickG++;
+                    lastG=i;
+                }
+                else if(ch=='M'){
+                    pickM++;
+                    lastM=i;
+                }
+            }
+
+           
+        }
+         for(int i=0;i<lastP;i++){
+                travelP +=travel[i];
+            }
+
+            for(int i=0;i<lastG;i++){
+                travelG +=travel[i];
+            }
+
+            for(int i=0;i<lastM;i++){
+                travelM +=travel[i];
+            }
+
+            int ans= (pickP+travelP)+(pickG+travelG)+(pickM+travelM);
+
+            return ans;
+    }
+};
