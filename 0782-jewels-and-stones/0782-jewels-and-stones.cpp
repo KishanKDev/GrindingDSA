168c736979c604c5118692c7af77e36d5d7388a2
Kishan Kumar <141938467+KishanKDev@users.noreply.github.com>
2025-01-19 23:54:00 +0530

    Time: 4 ms (1.94%), Space: 8.6 MB (35.75%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.cpp
+++ 0782-jewels-and-stones/0782-jewels-and-stones.cpp
@@ -1,16 +1,32 @@
+// class Solution {
+// public:
+//     int numJewelsInStones(string jewels, string stones) {
+//         int count=0;
+
+//         for(int i=0;i<jewels.length();i++){
+//             for(int j=0;j<stones.length();j++){
+//                 if(stones[j]==jewels[i]){
+//                     count++;
+//                 }
+//             }
+            
+//         }
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        int count=0;
+        unordered_set<char> jewelSet(jewels.begin(), jewels.end());
+        int count = 0;
 
-        for(int i=0;i<jewels.length();i++){
-            for(int j=0;j<stones.length();j++){
-                if(stones[j]==jewels[i]){
-                    count++;
-                }
+        for (char stone : stones) {
+            if (jewelSet.count(stone)) {
+                count++;
             }
-            
         }
+
         return count;
     }
 };
