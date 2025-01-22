9c2421ec17bdb74f7d3dc930bdba53bda04e4648
Kishan Kumar <141938467+KishanKDev@users.noreply.github.com>
2025-01-22 22:10:25 +0530

    Time: 0 ms (100%), Space: 33 MB (84.72%) - LeetHub

--- 3194-find-words-containing-character/3194-find-words-containing-character.cpp
+++ 3194-find-words-containing-character/3194-find-words-containing-character.cpp
@@ -16,3 +16,18 @@ public:
             return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> findWordsContaining(vector<string>& words, char x) {
+//         vector<int> ans;
+        
+//         for (int i = 0; i < words.size(); i++) {
+//             if (words[i].find(x) != string::npos) { // Efficient check if 'x' is present
+//                 ans.push_back(i);
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
