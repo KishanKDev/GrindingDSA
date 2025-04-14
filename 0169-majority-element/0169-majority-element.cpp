27f478cef08f2f5efd35dfeec184c56e9fc1f379
Kishan Kumar <141938467+KishanKDev@users.noreply.github.com>
2025-04-14 15:56:03 +0530

    Time: 0 ms (100%), Space: 28.2 MB (24.55%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -33,4 +33,14 @@ public:
 
         return -1;  
     }
+    //\U0001f525 Analogy: Election Battle \U0001f3af
+    //Socho tumhare paas ek line me log khade hain — kuch "Team A" ke supporter hain, kuch "Team B" ke.
+
+    //Jab bhi Team A ka supporter milta hai, tumhare paas ek banda add ho jaata hai (count++).
+
+    //Jab bhi Team B ka supporter milta hai, tumhara ek banda kam ho jaata hai (count--).
+
+    //Agar equal number me A aur B mile, to sab cancel ho jaate hain.
+
+    //⚠️ Lekin agar Team A ke log majority me hain, to end tak unke kuch log bach jaayenge. Wo bacha hua log majority element ko represent karta hai.
 };
