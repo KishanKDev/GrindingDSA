25ccaea31792dcfe63ecdce30cf06e3f5ce4fbe8
Kishan Kumar <141938467+KishanKDev@users.noreply.github.com>
2025-04-14 12:28:58 +0530

    Time: 0 ms (100%), Space: 27.9 MB (97.82%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,54 +1,36 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-      int n= nums.size();
+        int n= nums.size();
+        int count=0;
+        int element;
 
-      
+        for(int i=0;i<n;i++){
+            if(count==0){
+               count=1;
+               element=nums[i];
+            }
+            else if(nums[i]==element){
+               count++;
+            }
+            else{
+               count--;
+            }
 
-    //   for(int i=0;i<n;i++) {
-    //     int count= 0;
-    //     for(int j=0;j<n;j++){
-    //         if(nums[i]==nums[j]){
-    //             count++;
-    //         }
-    //     }
 
-    //     if(count>n/2){
-    //         return nums[i];
-    //     }
-    //   }
-
-    //   return -1;
-
-    int count=0;
-    int el;
-
-    for(int i=0;i<n;i++){
-        if(count==0){
-            count=1;
-            el=nums[i];
         }
-        else if(nums[i]==el){
-            count++;
-        }
-        else{
-            count--;
-        }
-
-
-    }
-    int count1=0;
-    for(int i=0;i<n;i++){
-        if(nums[i]==el){
-            count1++;
-        }
+        int count1=0;
+        for(int i=0;i<n;i++){
+           if(nums[i]==element){
+              count1++;
+           }
     
-    }
+        }
 
-    if(count1>n/2){
-        return el;
-    }
+        if(count1>n/2){
+           return element;
+        }
 
-    return -1;  
+        return -1;  
     }
 };
