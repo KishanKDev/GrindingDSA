a99f97af242c92747a37df7c537c3dfd627295e9
Kishan Kumar <141938467+KishanKDev@users.noreply.github.com>
2025-04-14 10:48:25 +0530

    Time: 2 ms (11.93%), Space: 30 MB (33.31%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,27 +3,47 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n= nums.size();
         if (n == 0)
-        return;
-         k = k % n;
-         if (k>n)
+    return;
+  k = k % n;
+  if (k > n)
+    return;
+  vector<int>temp(k);
+  for (int i = n - k; i < n; i++)
+  {
+    temp[i - n + k] = nums[i];
+  }
+  for (int i = n - k - 1; i >= 0; i--)
+  {
+    nums[i + k] = nums[i];
+  }
+  for (int i = 0; i < k; i++)
+  {
+    nums[i] = temp[i];
+  }
 
-         return;
+
+        // if (n == 0)
+        // return;
+        //  k = k % n;
+        //  if (k>n)
+
+        //  return;
 
 
-        //int temp[k]; 
+        // //int temp[k]; 
        
-        vector<int> temp(k);  // Corrected allocation size
-        for (int i = 0; i < k; i++) {
-            temp[i] = nums[n - k + i];  // Copy last k elements
-        }
+        // vector<int> temp(k);  // Corrected allocation size
+        // for (int i = 0; i < k; i++) {
+        //     temp[i] = nums[n - k + i];  // Copy last k elements
+        // }
 
-        for (int i = n - 1; i >= k; i--) {
-            nums[i] = nums[i - k];  // Shift elements to the right
-        }
+        // for (int i = n - 1; i >= k; i--) {
+        //     nums[i] = nums[i - k];  // Shift elements to the right
+        // }
 
-        for (int i = 0; i < k; i++) {
-            nums[i] = temp[i];  // Place stored elements at front
-        }
+        // for (int i = 0; i < k; i++) {
+        //     nums[i] = temp[i];  // Place stored elements at front
+        // }
 
         // vector<int>ans(n);
         
