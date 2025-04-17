70010fd6bed50ce753ab54c7b51f5e186083ebaa
Kishan Kumar <141938467+KishanKDev@users.noreply.github.com>
2025-04-17 13:08:30 +0530

    Time: 0 ms (100%), Space: 10.3 MB (35%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n= matrix.size();
-        // transpose nikalo aur phir reverse kro every row ko
+        int n= matrix.size(); //since it is square mat
+
+        // transpose karo aur phir Reverse kro each row ko
+        
         for(int i=0;i<n;i++){
-            for(int j=i;j<matrix[i].size();j++){
+            for(int j=i+1;j<n;j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
