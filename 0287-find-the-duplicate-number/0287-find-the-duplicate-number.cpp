d9ccf57dffa78fb4674ac7583ce71e6c133165f6
Kishan Kumar <141938467+KishanKDev@users.noreply.github.com>
2025-06-10 09:29:18 +0530

    Time: 1 ms (75.71%), Space: 65.1 MB (69.12%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,43 +1,28 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int n=nums.size();
-       
-        // Phase 1: Detect the intersection point of the cycle
-        int slow = nums[0];
-        int fast = nums[0];
+        // 1 3 4 2 2
+        // s
+        //       2   ye isliye kyunki shuru me hi loop break na ho
 
-        // do {
-        //     slow = nums[slow];       // Move slow pointer one step
-        //     fast = nums[nums[fast]]; // Move fast pointer two steps
-        // } while (slow != fast);
+        int slow =nums[0];
+        int fast =nums[0];
 
-        // // Phase 2: Find the entrance to the cycle (duplicate number)
-        // slow = nums[0];
-        // while (slow != fast) {
-        //     slow = nums[slow]; // Move both pointers one step
-        //     fast = nums[fast];
-        // }
+        slow=nums[slow];
+        fast = nums[nums[fast]];
 
-        // return slow; // The duplicate number
-
-        while (true) {
-            slow = nums[slow];       // Move slow pointer one step
-            fast = nums[nums[fast]]; // Move fast pointer two steps
-            if (slow == fast) {
-                break; // Exit the loop when the two pointers meet
-            }
+        //Detect cycle
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[nums[fast]];
         }
 
-        // Phase 2: Find the entrance to the cycle (duplicate number)
-        slow = nums[0];
-        while (slow != fast) {
-            slow = nums[slow]; // Move both pointers one step
-            fast = nums[fast];
+        slow=nums[0];
+
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
         }
-
-        return slow; // The duplicate number
-
-
+        return fast; //slow dono me se koi v
     }
 };
